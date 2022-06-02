c116e6f50c6152c2b027b28238501af9d1e95c78
reethika <reethika@umich.edu>
2022-06-02 19:01:04 -0400

    Commit proof of concept code for webserver and webpage

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # proxy-detection-server
 PoC for proxy/VPN detection project. Webserver code that runs an ICMP pinger and echo webserver that speaks WebSocket, with a test webpage that uses WebSockets to get RTT measurements
+
+The service is currently meant to be hosted at `https://test.reethika.info/ping` on an AWS instance hosted in Singapore
+
---
+++ cmd/test-server/main.go
@@ -0,0 +1,118 @@
+// Refer for webserver that speaks websocket: https://github.com/gorilla/websocket and for clientside websocket code:https://web.archive.org/web/20210614154432/https://incolumitas.com/2021/06/07/detecting-proxies-and-vpn-with-latencies/
+package main
+
+import (
+	"encoding/json"
+	"flag"
+	"github.com/go-ping/ping"
+	"github.com/gorilla/websocket"
+	"log"
+	"net"
+	"net/http"
+	"os"
+	"path"
+	"time"
+)
+
+var (
+	InfoLogger *log.Logger
+)
+var (
+	EchoLogger *log.Logger
+)
+
+type RtItem struct {
+	IP        string
+	PktSent   int
+	PktRecv   int
+	PktLoss   float64
+	MinRtt    time.Duration
+	AvgRtt    time.Duration
+	MaxRtt    time.Duration
+	StdDevRtt time.Duration
+}
+
+// Use with default options
+var upgrader = websocket.Upgrader{}
+
+func echo(w http.ResponseWriter, r *http.Request) {
+	c, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		EchoLogger.Println("upgrade:", err)
+		return
+	}
+	defer c.Close()
+	for {
+		mt, message, err := c.ReadMessage()
+		if err != nil {
+			EchoLogger.Println("read:", err)
+			break
+		}
+		log.Printf("recv: %s", message)
+		err = c.WriteMessage(mt, message)
+		if err != nil {
+			EchoLogger.Println("write:", err)
+			break
+		}
+	}
+}
+
+func pingSrv(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/ping" {
+		http.NotFound(w, r)
+		return
+	}
+	if r.Method != "GET" {
+		w.WriteHeader(http.StatusNotImplemented)
+		w.Write([]byte(http.StatusText(http.StatusNotImplemented)))
+		return
+	}
+	clientIPstr := r.RemoteAddr
+	clientIP, _, _ := net.SplitHostPort(clientIPstr)
+
+	pinger, err := ping.NewPinger(clientIP)
+	if err != nil {
+		panic(err)
+	}
+	pinger.Count = 3
+	pinger.Timeout = time.Second * 10
+	err = pinger.Run() // Blocks until finished.
+	if err != nil {
+		panic(err)
+	}
+	stats := pinger.Statistics() // get send/receive/duplicate/rtt stats
+	item := RtItem{clientIP, stats.PacketsSent, stats.PacketsRecv, stats.PacketLoss, stats.MinRtt, stats.AvgRtt, stats.MaxRtt, stats.StdDevRtt}
+	jsObj, err := json.Marshal(item)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	jsonString := string(jsObj)
+	http.ServeFile(w, r, "index.html")
+	InfoLogger.Println(jsonString)
+}
+
+func main() {
+	var logfilePath string
+	var echologPath string
+	flag.StringVar(&logfilePath, "logfile", "logFile.txt", "Path to log file")
+	flag.StringVar(&echologPath, "echolog", "echoLog.txt", "Path to echo server log file")
+	flag.Parse()
+	file, err := os.OpenFile(logfilePath, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0666)
+	if err != nil {
+		log.Fatal(err)
+	}
+	echoFile, err := os.OpenFile(echologPath, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0666)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	InfoLogger = log.New(file, "", log.Ldate|log.Ltime)
+	EchoLogger = log.New(echoFile, "", log.Ldate|log.Ltime)
+	certPath := "/etc/letsencrypt/live/test.reethika.info/"
+	fullChain := path.Join(certPath, "fullchain.pem")
+	privKey := path.Join(certPath, "privkey.pem")
+	http.HandleFunc("/ping", pingSrv)
+	http.HandleFunc("/echo", echo)
+	http.ListenAndServeTLS(":443", fullChain, privKey, nil)
+}
