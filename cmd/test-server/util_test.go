f4fa5fbb1d3a9e0302767b1bab7f29a83970972f
Reethika <reethika@umich.edu>
2022-08-19 16:43:47 -0400

    Add unit test for getSentTimestampfromIPId
    This test achieves full code coverage for this function that was missing from just zerotrace_test

--- cmd/test-server/util_test.go
+++ cmd/test-server/util_test.go
@@ -4,10 +4,12 @@ import (
 	"errors"
 	"github.com/stretchr/testify/assert"
 	"testing"
+	"time"
 )
 
 var (
 	invalidInputErr = errors.New("Invalid Input")
+	currentTime     = time.Now().UTC()
 )
 
 func TestValidateForm(t *testing.T) {
@@ -31,4 +33,25 @@ func TestValidateForm(t *testing.T) {
 	// Send empty input
 	_, err = validateForm("", "")
 	assert.Equal(t, invalidInputErr, err)
+}
+
+func TestGetSentTimestampfromIPId(t *testing.T) {
+	// Test the only untested portion of this function
+	sentPktsIPId := make(map[int][]SentPacketData)
+	sentPktsIPId[1] = append(sentPktsIPId[1], SentPacketData{HopIPId: 1, HopSentTime: currentTime})
+	sentPktsIPId[1] = append(sentPktsIPId[1], SentPacketData{HopIPId: 2, HopSentTime: currentTime})
+
+	// Retrieve the sent time for a valid IP ID from the slice that was passed
+	t1, err := getSentTimestampfromIPId(sentPktsIPId[1], 1)
+	if t1 != currentTime {
+		t.Fatalf("Expected to retrieve HopSentTime correctly, but got: %v", t1)
+	}
+	if err != nil {
+		t.Fatalf("Expected no error, but got %v", err)
+	}
+
+	// Retrieve the sent time for a invalid IP ID that does not exist in the slice that was passed
+	_, err = getSentTimestampfromIPId(sentPktsIPId[1], 1000)
+	assert.Error(t, errors.New("IP Id not in sent packets"), err)
+
 }
