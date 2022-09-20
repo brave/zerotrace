253e3eb2e7d6e18c2881c380e10a8bf6c9607f49
Philipp Winter <phw@brave.com>
2022-09-20 11:44:43 -0500

    Add a test."

--- zerotrace_test.go
+++ zerotrace_test.go
@@ -1,1 +1,12 @@
 package main
+
+import (
+	"testing"
+)
+
+func TestNewZeroTrace(t *testing.T) {
+	conn := &mockConn{}
+	if _, err := newZeroTrace("foobar", conn); err != nil {
+		t.Fatalf("Failed to create zerotrace object: %v", err)
+	}
+}
