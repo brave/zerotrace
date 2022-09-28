ad5c1fe771f2a72e18765c4fc74b01694654bb12
Philipp Winter <phw@brave.com>
2022-09-28 15:34:45 -0500

    Replace measurement study with simple example.

--- example/main.go
+++ example/main.go
@@ -1,6 +1,3 @@
-// Reference for webserver that speaks websocket: https://github.com/gorilla/websocket
-// Reference for client side websocket code:
-// https://web.archive.org/web/20210614154432/https://incolumitas.com/2021/06/07/detecting-proxies-and-vpn-with-latencies/
 package main
 
 import (
@@ -9,54 +6,128 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"text/template"
+	"time"
 
+	"github.com/brave-experiments/zerotrace"
 	"github.com/go-chi/chi"
-	"github.com/google/gopacket/pcap"
+	"github.com/gorilla/websocket"
 	"golang.org/x/crypto/acme/autocert"
 )
 
-const (
-	ifaceNameAny = "any"
-)
-
 var (
-	l = log.New(os.Stderr, "latsrv: ", log.Ldate|log.Ltime|log.LUTC|log.Lshortfile)
+	l = log.New(os.Stderr, "example: ", log.Ldate|log.Ltime|log.LUTC|log.Lshortfile)
 )
 
-// hasAnyInterface returns true if the system has a networking interface called
-// "any".
-func hasAnyInterface() bool {
-	ifaces, err := pcap.FindAllDevs()
-	if err != nil {
-		return false
-	}
+func getIdxHandler(domain, addr string) http.HandlerFunc {
+	idxPage := `
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset = "utf-8">
+    <title>ZeroTrace test</title>
+  </head>
+  <body>
+    <p>Status: <span id="status">Running</span></p>
+    <script>
+      function getLatencyWebSocket(endpoint) {
+        return new Promise(function(resolve, reject) {
+          var socket = new WebSocket(endpoint);
+          socket.onerror = function (err) {
+            reject(err.toString());
+          }
+          socket.onclose = function(event) {
+            resolve();
+          }
+          socket.onmessage = function(event) {
+            socket.send(event.data);
+          }
+        });
+      }
+      getLatencyWebSocket("wss://{{.WssEndpoint}}/wss").then(() => {
+        document.getElementById("status").innerHTML = "Done.";
+      });
+    </script>
+  </body>
+</html>`
+	idxTemplate := template.Must(template.New("idx").Parse(idxPage))
 
-	l.Print("Iterating over interfaces:")
-	for _, iface := range ifaces {
-		l.Printf("\t- %s", iface.Name)
-		if iface.Name == ifaceNameAny {
-			return true
+	return func(w http.ResponseWriter, r *http.Request) {
+		s := struct {
+			WssEndpoint string
+		}{
+			WssEndpoint: domain + addr,
+		}
+		if err := idxTemplate.Execute(w, s); err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
 		}
 	}
-	return false
+}
+
+func wssHandler(w http.ResponseWriter, r *http.Request) {
+	l.Println("Handling new WebSocket request.")
+
+	var upgrader = websocket.Upgrader{
+		CheckOrigin: func(r *http.Request) bool {
+			return true
+		},
+	}
+	c, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	defer c.Close()
+	l.Println("Successfully upgraded request to WebSocket.")
+
+	done := make(chan bool)
+	// Start 0trace measurement in the background.
+	go func() {
+		myConn := c.UnderlyingConn()
+		zt := zerotrace.NewZeroTrace(zerotrace.NewDefaultConfig())
+		rtt, err := zt.CalcRTT(myConn)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+		}
+		l.Printf("Round trip time to client: %dms", rtt.Milliseconds())
+		close(done)
+	}()
+
+	// Keep the client around while the measurement is running because we need
+	// to take advantage of the already-established TCP connection.
+	for {
+		select {
+		case <-done:
+			l.Println("0trace measurement is done.")
+			return
+		case <-time.Tick(time.Second):
+			if err := c.WriteMessage(websocket.TextMessage, []byte("ping")); err != nil {
+				l.Printf("Error writing message to WebSocket conn: %v", err)
+			}
+		}
+	}
 }
 
 func main() {
-	var addr, ifaceName string
-	flag.StringVar(&ifaceName, "iface", ifaceNameAny, "Interface name to listen on, default: any")
-	flag.StringVar(&addr, "addr", ":8080", "Address to listen on, default: :8080")
+	var addr, domain, ifaceName string
+	flag.StringVar(&ifaceName, "iface", "eth0", "Network interface name to listen on (default: eth0)")
+	flag.StringVar(&addr, "addr", ":8443", "Address to listen on (default: :8443)")
+	flag.StringVar(&domain, "domain", "", "The Web server's domain name.")
 	flag.Parse()
 
+	if domain == "" {
+		l.Fatal("Specify domain name by using the -domain flag.")
+	}
+
 	router := chi.NewRouter()
-	router.Get("/", indexHandler)
-	router.Get("/ping", pingHandler)
-	router.Get("/echo", echoHandler)
-	router.Get("/trace", traceHandler)
-	router.Post("/measure", measureHandler)
+	router.Get("/wss", wssHandler)
+	router.Get("/", getIdxHandler(domain, addr))
 
 	certManager := autocert.Manager{
-		Prompt: autocert.AcceptTOS,
-		Cache:  autocert.DirCache("certs"),
+		Prompt:     autocert.AcceptTOS,
+		Cache:      autocert.DirCache("certs"),
+		HostPolicy: autocert.HostWhitelist(domain),
 	}
 	go http.ListenAndServe(":http", certManager.HTTPHandler(nil)) //nolint:errcheck
 	server := &http.Server{
@@ -65,10 +136,6 @@ func main() {
 		TLSConfig: &tls.Config{
 			GetCertificate: certManager.GetCertificate,
 		},
-	}
-
-	if ifaceName == ifaceNameAny && !hasAnyInterface() {
-		l.Fatal("We were told to use the 'any' interface but it's not present.")
 	}
 
 	l.Printf("Starting Web service to listen on %s.", addr)
